bcf9605edda7b7c75d1bcfa4ce5f3bb8d89f6296
ChristopheKouekem <chriskou.ck78@gmail.com>
2025-04-09 10:19:41 +0200

    Target Spawnt jetzt random

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    public Vector2 respawnRangeMin = new Vector2(-1, -1);
+    public Vector2 respawnRangeMax = new Vector2(1, 1);
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Feuerball"))
         {
             Destroy(collision.gameObject);
-            Destroy(gameObject);
-        }
-
-        if ()
-        {
-
+            NeuesTarget();
         }
     }
+
+    void NeuesTarget()
+    {
+        float newX = Random.Range(respawnRangeMin.x, respawnRangeMax.x);
+        float newY = Random.Range(respawnRangeMin.y, respawnRangeMax.y);
+        transform.position = new Vector2(newX, newY);
+    }
+
+
 }
 
