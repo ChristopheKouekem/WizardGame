df0588ec92fb57d596374ead1ac56343645286b8
ChristopheKouekem <chriskou.ck78@gmail.com>
2025-06-11 11:09:17 +0200

    Singelton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public enum GameState { MainMenu, Play, Paused, GameOver }
+    public GameState state;
+    public static GameManager instance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            state = GameState.Play;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
